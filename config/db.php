dd2d2c1ffc49c34c1ad74f67c2d3ba4ec6d26fdb
Dmitry <kuchura.d@gmail.com>
2017-07-26 08:28:59 +0300

    init yii2 application

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=91.200.60.68;dbname=infocorpus_db',
+    'username' => 'infocorpus_db',
+    'password' => '8L8p6I4v',
+    'charset' => 'utf8',
+];
