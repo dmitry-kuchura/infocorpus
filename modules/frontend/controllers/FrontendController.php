a878ff6c99021fd1f745288dbbfb022b5df8962c
Dmitry <kuchura.d@gmail.com>
2017-08-30 15:04:54 +0300

    some changes

--- modules/frontend/controllers/FrontendController.php
+++ modules/frontend/controllers/FrontendController.php
@@ -376,6 +376,11 @@ class FrontendController extends Controller
         };
     }
 
+    /**
+     * Создание клиента
+     *
+     * @return array
+     */
     public function actionCreateCustomer()
     {
         $symbol = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuwxyz0123456789';
@@ -394,7 +399,7 @@ class FrontendController extends Controller
             $model->organization = $data['company'];
             $model->location = $data['company-location'];
             $model->car_name = $data['car'];
-            $model->car_color= $data['car-color'];
+            $model->car_color = $data['car-color'];
             $model->car_number = $data['car-number'];
             $model->password = substr(str_shuffle(str_repeat($symbol, 8)), 0, 10);
             $model->status = 1;
@@ -411,5 +416,33 @@ class FrontendController extends Controller
                 ];
             }
         }
+    }
+
+    /**
+     * Получение списка клиентов
+     *
+     * @return array
+     */
+    public function actionListCustomers()
+    {
+        /* @var $result Users */
+        $result = Users::find()->where(['role' => 0])->all();
+
+        foreach ($result as $obj) {
+            $customers[] = [
+                'id' => $obj->id,
+                'name' => $obj->username,
+                'phone' => $obj->phone,
+                'imei' => $obj->imei,
+                'identity' => $obj->auth_key,
+                'location' => $obj->address,
+                'status' => $obj->status,
+            ];
+        }
+
+        return [
+            'success' => true,
+            'customers' => $customers
+        ];
     }
 }
