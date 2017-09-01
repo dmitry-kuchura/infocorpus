e22d16edc62c9a5e21d2d8707f343e4bd54d584e
Dmitry <kuchura.d@gmail.com>
2017-09-01 11:44:48 +0300

    change routing

--- config/routing.php
+++ config/routing.php
@@ -2,14 +2,14 @@
 
 return [
     /* Groups actions */
-    'frontend/group-create' => 'frontend/group/group-create',
-    'frontend/group-list' => 'frontend/group/group-list',
-    'frontend/group-alert' => 'frontend/group/group-alert',
-    'frontend/group-change-status' => 'frontend/group/group-change-status',
-    'frontend/group-change-allow' => 'frontend/group/group-change-allow',
-    'frontend/group-delete' => 'frontend/group/group-delete',
-    'frontend/group-cancel' => 'frontend/group/group-cancel',
-    'frontend/group-send-message' => 'frontend/group/group-send-message',
+    'frontend/group-create' => 'frontend/groups/group-create',
+    'frontend/group-list' => 'frontend/groups/group-list',
+    'frontend/group-alert' => 'frontend/groups/group-alert',
+    'frontend/group-change-status' => 'frontend/groups/group-change-status',
+    'frontend/group-change-allow' => 'frontend/groups/group-change-allow',
+    'frontend/group-delete' => 'frontend/groups/group-delete',
+    'frontend/group-cancel' => 'frontend/groups/group-cancel',
+    'frontend/group-send-message' => 'frontend/groups/group-send-message',
 
     'frontend/<action>' => 'frontend/frontend/<action>',
     'api/<action>' => 'api/api/<action>',
